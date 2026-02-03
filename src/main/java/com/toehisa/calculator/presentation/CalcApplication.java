31a4b4a16f02f2f4d2b1f3fac2d49b50a33c7c95
Toehisa <tigrera9999eagle@gmail.com>
2026-02-03 20:20:25 +0500

    [presentation]: Create adaptiveScene method, for dynamic app window size.

--- src/main/java/com/toehisa/calculator/presentation/CalcApplication.java
+++ src/main/java/com/toehisa/calculator/presentation/CalcApplication.java
@@ -1,6 +1,7 @@
 package com.toehisa.calculator.presentation;
 
 
+import com.toehisa.calculator.data.SysInfoProvider.ScreenResolution;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,12 +11,31 @@ import java.io.IOException;
 
 public class CalcApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         FXMLLoader fxmlLoader = new FXMLLoader(CalcApplication.class.getResource("calculator-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),200, 200);
+        ScreenResolution resInfo = new ScreenResolution();
+
+        Scene scene = adaptiveScene(
+                fxmlLoader,
+                resInfo.getScreenWidth(0),
+                resInfo.getScreenHeight(0)
+        );
 
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private <T> Scene adaptiveScene(FXMLLoader loader, double width, double height) {
+        try {
+            if (width < height) {
+                return new Scene(loader.load(), width / 9, height / 6);
+            } else {
+                return new Scene(loader.load(), height / 6, width / 9);
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return null;
     }
 }
