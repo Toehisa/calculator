92d4c95fb9a96141c0454ad3cf2707b92f3397f3
Toehisa <tigrera9999eagle@gmail.com>
2026-02-10 02:39:47 +0500

    [presentation]: CalcApplication app sizes update.

--- src/main/java/com/toehisa/calculator/presentation/CalcApplication.java
+++ src/main/java/com/toehisa/calculator/presentation/CalcApplication.java
@@ -7,35 +7,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class CalcApplication extends Application {
+    public static  double SCREEN_WIDTH = ScreenResolution.getScreenWidth(0);
+    public static  double SCREEN_HEIGHT = ScreenResolution.getScreenHeight(0);
     @Override
     public void start(Stage stage) {
-        FXMLLoader fxmlLoader = new FXMLLoader(CalcApplication.class.getResource("calculator-view.fxml"));
-        ScreenResolution resInfo = new ScreenResolution();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("calculator-view.fxml"));
+        Scene scene = adaptiveScene(fxmlLoader, SCREEN_WIDTH, SCREEN_HEIGHT, 5);
 
-        Scene scene = adaptiveScene(
-                fxmlLoader,
-                resInfo.getScreenWidth(0),
-                resInfo.getScreenHeight(0)
-        );
+        CalcController.vBoxHeight = scene.getHeight();
+        CalcController.vBoxWidth = scene.getWidth();
 
-        stage.setTitle("Calculator");
+        stage.setResizable(false);
+        stage.setTitle("calculator");
         stage.setScene(scene);
+
         stage.show();
+
     }
 
-    private Scene adaptiveScene(FXMLLoader loader, double width, double height) {
+    private Scene adaptiveScene(FXMLLoader loader, double width, double height, int multiplier) {
         try {
             if (width < height) {
-                return new Scene(loader.load(), width / 9, height / 6);
+                return new Scene(loader.load(), width / multiplier, height / multiplier);
             } else {
-                return new Scene(loader.load(), height / 6, width / 9);
+                return new Scene(loader.load(), height / multiplier, width / multiplier);
             }
         } catch (IOException e) {
             System.err.println("adaptiveSceneErr:" + e);
         }
         return null;
     }
+
+
+
 }
